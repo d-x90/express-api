76607c1ccfd47bc792d6d319a6aaf6631c612be6
d-x90 <work.dhorvath@gmail.com>
2021-01-09 19:43:37 +0100

    Rename folder of unit tests

--- backend/unit-tests/.test-variables.js
+++ backend/unit-tests/.test-variables.js
@@ -2,3 +2,4 @@ process.env.NODE_ENV = 'test';
 process.env.DB_LOG_SQL_TO_OUTPUT_ENABLED = 'false';
 process.env.JWT_SIGN_KEY = 'longsecretkey';
 process.env.LOGGING_ENABLED = 'false';
+process.env.DB_CONNECTION_STRING = 'sqlite::memory';
